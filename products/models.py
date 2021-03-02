15d3a121eb9b5b1f0fefe02302cd87775885f2f5
kristi0403 <kristirapi@hotmail.it>
2021-03-02 23:02:36 +0000

    added products app, models and fixtures

--- products/models.py
+++ products/models.py
@@ -2,9 +2,6 @@ from django.db import models
 
 
 class Category(models.Model):
-
-    class Meta:
-        verbose_name_plural = 'Categories'
     name = models.CharField(max_length=254)
     friendly_name = models.CharField(max_length=254, null=True, blank=True)
 
@@ -14,9 +11,8 @@ class Category(models.Model):
     def get_friendly_name(self):
         return self.friendly_name
 
-
 class Product(models.Model):
-    category = models.ForeignKey('Category', null=True, blank=True,on_delete=models.SET_NULL)
+    category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
     sku = models.CharField(max_length=254, null=True, blank=True)
     name = models.CharField(max_length=254)
     description = models.TextField()
@@ -24,6 +20,7 @@ class Product(models.Model):
     rating = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)
     image_url = models.URLField(max_length=1024, null=True, blank=True)
     image = models.ImageField(null=True, blank=True)
+
 
     def __str__(self):
         return self.name
