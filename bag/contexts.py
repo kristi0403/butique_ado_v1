3f49567bf2e880ea7f0b5a68c113fe3256b028ad
kristi0403 <kristirapi@hotmail.it>
2021-03-06 20:49:50 +0000

    added sizes to product model and size selector box to product template

--- bag/contexts.py
+++ bag/contexts.py
@@ -3,7 +3,6 @@ from django.conf import settings
 from django.shortcuts import get_object_or_404
 from products.models import Product
 
-
 def bag_contents(request):
 
     bag_items = []
@@ -18,7 +17,7 @@ def bag_contents(request):
         bag_items.append({
             'item_id': item_id,
             'quantity': quantity,
-            'product': product
+            'product': product,
         })
 
     if total < settings.FREE_DELIVERY_THRESHOLD:
